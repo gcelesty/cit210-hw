68f5b1b412f33f21994e26d7de18a9bf55adcd1d
curlygirly <geo18005@byui.edu>
2023-01-11 13:12:15 -0600

    added

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic = randomGenerator.Next(1, 101);
+
+        int guess = -1; 
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess  = int.Parse(Console.ReadLine());
+
+            if (magic > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        } while (guess != magic);
     }
 }
