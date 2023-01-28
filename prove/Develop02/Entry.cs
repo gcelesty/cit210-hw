7218bed2ed5d0d00d1005c89263e2b34247fb6d8
curlygirly <geo18005@byui.edu>
2023-01-28 17:00:00 -0600

    added

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -4,8 +4,12 @@ class Entry
     public string _prompt;
     public string _text;
 
-    public void Display()
+    public void Display(string prompt, string response)
     {
         // display the details of the entry
+        this._date = DateTime.Now.ToString();
+        this._prompt = prompt;
+        this._text = response;
+        
     }
 }
