343c69dd050158229686bff0395ef4eb36902664
curlygirly <geo18005@byui.edu>
2023-01-28 19:57:43 -0600

    added

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -4,6 +4,21 @@ class Entry
     public string _prompt;
     public string _text;
 
+    public Entry()
+    {
+    }
+
+    public Entry(string v1, string v2, string v3)
+    {
+        V1 = v1;
+        V2 = v2;
+        V3 = v3;
+    }
+
+    public string V1 { get; }
+    public string V2 { get; }
+    public string V3 { get; }
+
     public void Display(string date, string prompt, string response)
     {
         // display the details of the entry
